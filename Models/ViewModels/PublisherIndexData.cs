027afd5051b8576219003d6ebf266910d3bdf8df
astoica94 <alex.stoica94@gmail.com>
2023-11-12 20:04:27 +0200

    lab 4 part 1

---
+++ Models/ViewModels/PublisherIndexData.cs
@@ -0,0 +1,8 @@
+﻿namespace Stoica_Alexandru_Lab2.Models.ViewModels
+{
+    public class PublisherIndexData
+    {
+        public IEnumerable<Publisher> Publishers { get; set; }
+        public IEnumerable<Book> Books { get; set; }
+    }
+}
